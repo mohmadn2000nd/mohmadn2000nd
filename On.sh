ed0efa621267d0807ee999cdde6afed25ff17ee9
mohmadn2000nd <80320858+mohmadn2000nd@users.noreply.github.com>
2021-11-15 12:37:52 +0300

    Add files via upload

---
+++ On.sh
@@ -0,0 +1,156 @@
+rm -rf /data/data/com.tencent.ig/app_crashrecord
+touch /data/data/com.tencent.ig/app_crashrecord
+rm -rf /data/data/com.tencent.ig/databases
+rm -rf /data/data/com.tencent.ig/files/ano_tmp/*
+chmod -R 000 /data/data/com.tencent.ig/files/ano_tmp
+rm -rf /data/data/com.tencent.ig/*cache*
+rm -rf /data/data/com.tencent.ig/app*
+rm -rf /storage/emulated/0/.backups
+rm -rf /storage/emulated/0/MidasOversea
+rm -rf /data/data/com.tencent.ig/*app*
+rm -rf /data/data/com.tencent.ig/cache
+rm -rf /data/data/com.tencent.ig/code_cache
+rm -rf /data/data/com.tencent.ig/no_backup
+touch /data/data/com.tencent.ig/app_appcache
+touch /data/data/com.tencent.ig/app_bugly
+touch /data/data/com.tencent.ig/app_crashrecord
+touch /data/data/com.tencent.ig/app_databases
+touch /data/data/com.tencent.ig/app_geolocation
+touch /data/data/com.tencent.ig/app_lib,app_textures,cache
+touch /data/data/com.tencent.ig/code_cache
+touch /data/data/com.tencent.ig/no_backup
+chmod -R 0000 /data/data/com.tencent.ig/app_appcache
+chmod -R 0000 /data/data/com.tencent.ig/app_bugly
+chmod -R 0000 /data/data/com.tencent.ig/app_crashrecord
+chmod -R 0000 /data/data/com.tencent.ig/app_databases
+chmod -R 0000 /data/data/com.tencent.ig/app_geolocation
+chmod -R 0000 /data/data/com.tencent.ig/app_lib
+chmod -R 0000 /data/data/com.tencent.ig/app_textures
+chmod -R 0000 /data/data/com.tencent.ig/cache
+chmod -R 0000 /data/data/com.tencent.ig/code_cache
+chmod -R 0000 /data/data/com.tencent.ig/no_backup
+rm -rf /data/data/com.tencent.ig/databases/*
+touch /data/data/com.tencent.ig/databases/__hs__db_issues
+touch /data/data/com.tencent.ig/databases/__hs__db_issues-shm
+touch /data/data/com.tencent.ig/databases/__hs__db_issues-wal
+touch /data/data/com.tencent.ig/databases/__hs__db_key_values
+touch /data/data/com.tencent.ig/databases/__hs__db_key_values-shm
+touch /data/data/com.tencent.ig/databases/__hs__db_key_values-wal
+touch /data/data/com.tencent.ig/databases/__hs__db_support_key_values
+touch /data/data/com.tencent.ig/databases/__hs__db_support_key_values-shm
+touch /data/data/com.tencent.ig/databases/__hs__db_support_key_values-wal
+touch /data/data/com.tencent.ig/databases/__hs__db_support_key_values
+touch /data/data/com.tencent.ig/databases/__hs__db_support_key_values-shm
+touch /data/data/com.tencent.ig/databases/__hs__db_support_key_values-wal
+touch /data/data/com.tencent.ig/databases/__hs_log_store
+touch /data/data/com.tencent.ig/databases/__hs_log_store-shm
+touch /data/data/com.tencent.ig/databases/__hs_log_store-wal
+touch /data/data/com.tencent.ig/databases/bugly_db_
+touch /data/data/com.tencent.ig/databases/bugly_db_-shm
+touch /data/data/com.tencent.ig/databases/bugly_db_-wal
+touch /data/data/com.tencent.ig/databases/config.db
+touch /data/data/com.tencent.ig/databases/google_app_measurement_local.db
+touch /data/data/com.tencent.ig/databases/iMSDK.db
+chmod -R 0000 /data/data/com.tencent.ig/databases/__hs__db_issues
+chmod -R 0000 /data/data/com.tencent.ig/databases/__hs__db_issues-shm
+chmod -R 0000 /data/data/com.tencent.ig/databases/__hs__db_issues-wal
+chmod -R 0000 /data/data/com.tencent.ig/databases/__hs__db_key_values
+chmod -R 0000 /data/data/com.tencent.ig/databases/__hs__db_key_values-shm
+chmod -R 0000 /data/data/com.tencent.ig/databases/__hs__db_key_values-wal
+chmod -R 0000 /data/data/com.tencent.ig/databases/__hs__db_support_key_values
+chmod -R 0000 /data/data/com.tencent.ig/databases/__hs__db_support_key_values-shm
+chmod -R 0000 /data/data/com.tencent.ig/databases/__hs__db_support_key_values-wal
+chmod -R 0000 /data/data/com.tencent.ig/databases/__hs__db_support_key_values
+chmod -R 0000 /data/data/com.tencent.ig/databases/__hs__db_support_key_values-shm
+chmod -R 0000 /data/data/com.tencent.ig/databases/__hs__db_support_key_values-wal
+chmod -R 0000 /data/data/com.tencent.ig/databases/__hs_log_store
+chmod -R 0000 /data/data/com.tencent.ig/databases/__hs_log_store-shm
+chmod -R 0000 /data/data/com.tencent.ig/databases/__hs_log_store-wal
+chmod -R 0000 /data/data/com.tencent.ig/databases/bugly_db_-shm
+chmod -R 0000 /data/data/com.tencent.ig/databases/bugly_db_-wal
+chmod -R 0000 /data/data/com.tencent.ig/databases/config.db
+chmod -R 0000 /data/data/com.tencent.ig/databases/google_app_measurement_local.db
+chmod -R 0000 /data/data/com.tencent.ig/databases/iMSDK.db
+rm -rf /data/media/legacy
+touch /data/media/legacy
+chmod -R 0000 /data/media/legacy
+rm -rf /storage/emulated/0/.backups
+rm -rf /storage/emulated/0/.ext4
+rm -rf /storage/emulated/0/.sstmp
+rm -rf /storage/emulated/0/MidasOversea
+rm -rf /storage/emulated/0/QTAudioEngine
+rm -rf /storage/emulated/0/tencent
+touch /storage/emulated/0/.backups
+touch /storage/emulated/0/.ext4
+touch /storage/emulated/0/.sstmp
+chmod -R 0000 /storage/emulated/0/.backups
+chmod -R 0000 /storage/emulated/0/.ext4
+chmod -R 0000 /storage/emulated/0/.sstmp
+chmod -R 0000 /storage/emulated/0/MidasOversea
+chmod -R 0000 /storage/emulated/0/QTAudioEngine
+chmod -R 0000 /storage/emulated/0/tencent
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/cache
+touch /storage/emulated/0/Android/data/com.tencent.ig/cache
+chmod -R 0000 /storage/emulated/0/Android/data/com.tencent.ig/cache
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/TGPA
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/ca-bundle.pem
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/cacheFile.txt
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/hawk_data
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/login-identifier.txt
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/TGPA
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/ca-bundle.pem
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/cacheFile.txt
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/hawk_data
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/login-identifier.txt
+chmod -R 0000 /storage/emulated/0/Android/data/com.tencent.ig/files/TGPA
+chmod -R 0000 /storage/emulated/0/Android/data/com.tencent.ig/files/ca-bundle.pem
+chmod -R 0000 /storage/emulated/0/Android/data/com.tencent.ig/files/cacheFile.txt
+chmod -R 0000 /storage/emulated/0/Android/data/com.tencent.ig/files/hawk_data
+chmod -R 0000 /storage/emulated/0/Android/data/com.tencent.ig/files/login-identifier.txt
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/Engine
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/Epic Games
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/Engine
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/Epic Games
+chmod -R 0000 /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/Engine
+chmod -R 0000 /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/Epic Games
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Intermediate
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Intermediate
+chmod -R 0000 /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Intermediate
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/MMKV
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferTmpDir
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/rawdata
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/RoleInfo
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/TableDatas
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/MMKV
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/rawdata
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/RoleInfo
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/TableDatas
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo
+chmod -R 0000 /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
+chmod -R 0000 /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/MMKV
+chmod -R 0000 /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/rawdata
+chmod -R 0000 /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/RoleInfo
+chmod -R 0000 /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/TableDatas
+chmod -R 0000 /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo
+rm -rf /data/data/com.tencent.ig/lib/libapp.so
+rm -rf /data/data/com.tencent.ig/lib/libBugly.so
+rm -rf /data/data/com.tencent.ig/lib/libc++_shared.so
+rm -rf /data/data/com.tencent.ig/lib/libflutter.so
+rm -rf /data/data/com.tencent.ig/lib/libgamemaster.so
+rm -rf /data/data/com.tencent.ig/lib/libgcloudarch.so
+rm -rf /data/data/com.tencent.ig/lib/libhelpshiftlistener.so
+rm -rf /data/data/com.tencent.ig/lib/libigshare.so
+rm -rf /data/data/com.tencent.ig/lib/libImSDK.so
+rm -rf /data/data/com.tencent.ig/lib/libkk-image.so
+rm -rf /data/data/com.tencent.ig/lib/liblbs.so
+rm -rf /data/data/com.tencent.ig/lib/libnpps-jni.so
+rm -rf /data/data/com.tencent.ig/lib/libsentry.so
+rm -rf /data/data/com.tencent.ig/lib/libsentry-android.so
+rm -rf /data/data/com.tencent.ig/lib/libsoundtouch.so
+rm -rf /data/data/com.tencent.ig/lib/libst-engine.so
+rm -rf /data/data/com.tencent.ig/lib/libtgpa.so
+rm -rf /data/data/com.tencent.ig/lib/libzip.so
+ 
